7a39a91b41d8ee72c793528b0929af7f952a471d
Khai Nguyen <104447228+Khainq1911@users.noreply.github.com>
2025-03-19 16:22:54 +0700

    feat: header and logout
    Co-authored-by: KhaiNguyen <khaitom1911@Khai.nq213964sis.hust.edu.vn>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,43 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { ListItemIcon, Menu, MenuItem } from "@mui/material";
+import Logout from "@mui/icons-material/Logout";
+import { useState } from "react";
+
 export default function Header() {
-  return <div className="w-screen h-[60px] bg-amber-50 fixed top-0 left-0">Header</div>;
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    window.location.href = "/login";
+  };
+  return (
+    <div className="w-screen h-[60px] bg-[#4682B4] fixed top-0 left-0 px-5">
+      <div className="sm:hidden lg:hidden float-left leading-[60px]">
+        <MenuIcon fontSize="large" className="cursor-pointer text-[#fff]" />
+      </div>
+      <div className="float-right leading-[60px]">
+        <AccountCircleIcon
+          fontSize="large"
+          className="cursor-pointer text-[#fff]"
+          onClick={handleClick}
+        />
+      </div>
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <MenuItem onClick={logout}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </div>
+  );
 }
